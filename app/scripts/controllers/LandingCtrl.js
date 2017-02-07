1480aa5d6153bbc33c2640e95c9fc2e34b3431c6
Frederick Lazo <fredlazo@yahoo.com>
2017-02-07 14:26:23 -0800

    Assignment-5 completed

--- app/scripts/controllers/LandingCtrl.js
+++ app/scripts/controllers/LandingCtrl.js
@@ -11,6 +11,25 @@
 
 /*
 
+
+ (function() {
+     function CollectionCtrl() {
+         this.albums = [];
+         for (var i=0; i < 12; i++) {
+             this.albums.push(angular.copy(albumPicasso));
+         }         
+     }
+ 
+     angular
+         .module('blocJams')
+         .controller('CollectionCtrl', CollectionCtrl);
+ })();
+
+
+
+
+
+
 var app = angular.module('blocJams',[]);
 
 app.controller('LandingCtrl', ['http', '$log', function($http, $log){
