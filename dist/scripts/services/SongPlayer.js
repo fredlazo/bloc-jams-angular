7a13d3cd312d916cdf0e91ce1a890223048bf962
Frederick Lazo <fredlazo@yahoo.com>
2017-02-07 17:34:02 -0800

    Assignment-7 completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -35,12 +35,27 @@
       currentSong = song;
     };
     
-    
+    /**
+    * @function playSong
+    * @desc Plays the loaded currentBuzzObject
+    * @param {Object} song
+    */
+      
+    var playSong = function(song) {
+      currentBuzzObject.play();
+      song.playing = true;
+    };
+
+    /**
+    * @function SongPlayer.play (public method of the SongPlayer service)
+    * @desc Uses the private setSong and playSong methods to play music one at a time
+    * @param {Object} song
+    */
+      
     SongPlayer.play = function(song) {
       if (currentSong !== song) {
           setSong(song);
-          currentBuzzObject.play();
-          song.playing = true;
+          playSong(song);
       }
       else if (currentSong === song) {
         if (currentBuzzObject.isPaused()) {
@@ -50,6 +65,12 @@
 
     };
 
+    /**
+    * @function SongPlayer.pause (public method of the SongPlayer service)
+    * @desc Uses the private setSong and playSong methods to pause currently playing music
+    * @param {Object} song
+    */
+      
     SongPlayer.pause = function(song) {
       currentBuzzObject.pause();
       song.playing = false;
