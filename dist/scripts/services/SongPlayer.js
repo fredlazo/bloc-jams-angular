a410ef5cb9eba53fd5aef1ce4b05664eb20c0072
Frederick Lazo <fredlazo@yahoo.com>
2017-02-07 21:09:25 -0800

    Checkpoint-10 completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
     /**
@@ -21,20 +21,25 @@
     */
     var setSong = function(song) {
 
-    if (currentBuzzObject) {
-      currentBuzzObject.stop();
+        if (currentBuzzObject) {
+          currentBuzzObject.stop();
 
-      if (SongPlayer.currentSong)
-      {
-        SongPlayer.currentSong.playing = null;
-      }
-      
-    }
+          if (SongPlayer.currentSong)
+          {
+            SongPlayer.currentSong.playing = null;
+          }
+        }
  
     currentBuzzObject = new buzz.sound(song.audioUrl, {
       formats: ['mp3'],
       preload: true
     });
+        
+    currentBuzzObject.bind('timeupdate', function() {
+         $rootScope.$apply(function() {
+             SongPlayer.currentTime = currentBuzzObject.getTime();
+         });
+     });    
  
       SongPlayer.currentSong = song;
     };
@@ -78,6 +83,12 @@
     * @type {Object}
     */
     SongPlayer.currentSong = null;
+      
+     /**
+     * @desc Current playback time (in seconds) of currently playing song
+     * @type {Number}
+     */
+     SongPlayer.currentTime = null;  
 
     /**
     * @desc Current album variable (public)
@@ -154,11 +165,22 @@
       }
 
     };
-    
+      
+         /**
+     * @function setCurrentTime
+     * @desc Set current time (in seconds) of currently playing song
+     * @param {Number} time
+     */
+     SongPlayer.setCurrentTime = function(time) {
+         if (currentBuzzObject) {
+             currentBuzzObject.setTime(time);
+         }
+     };  
+
     return SongPlayer;
   }
  
     angular
       .module('blocJams')
-      .factory('SongPlayer', SongPlayer);
+      .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
